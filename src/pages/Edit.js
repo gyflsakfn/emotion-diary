60c2f47049ea13eb83cb306d0f369d59d76bead7
JEONGMIN <gyflsakfn@naver.com>
2022-10-01 20:40:11 +0900

    route-test

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,8 +1,36 @@
+import { useSearchParams, useNavigate } from "react-router-dom";
+
 const Edit = () => {
+  const navigate = useNavigate();
+  const [searchParams, setSearchparams] = useSearchParams();
+
+  const id = searchParams.get("id");
+  console.log("id: ", id);
+
+  const mode = searchParams.get("mode");
+  console.log("mode: ", mode);
+
   return (
     <div>
       <h1>Edit</h1>
       <p>이곳은 일기 수정 페이지 입니다.</p>
+      <button onClick={() => setSearchparams({ who: "jeongmin" })}>
+        QS 바꾸기
+      </button>
+      <button
+        onClick={() => {
+          navigate("/home");
+        }}
+      >
+        홈으로 가기
+      </button>
+      <button
+        onClick={() => {
+          navigate(-1);
+        }}
+      >
+        뒤로 가기
+      </button>
     </div>
   );
 };
