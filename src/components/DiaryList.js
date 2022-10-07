893f2e5ae7ed1435700f64e699088412cee56723
JEONGMIN <gyflsakfn@naver.com>
2022-10-07 20:30:20 +0900

    filter implement

--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -1,8 +1,25 @@
 import { useState } from "react";
 
+// 최신&오래된 순 필터 리스트
 const sortOptionList = [
   { value: "latest", name: "최신순" },
   { value: "oldest", name: "오래된순" },
+];
+
+// 감정 필터
+const filterOptionList = [
+  {
+    value: "all",
+    name: "전부다",
+  },
+  {
+    value: "good",
+    name: "좋은 감정만",
+  },
+  {
+    value: "bad",
+    name: "안좋은 감정만",
+  },
 ];
 
 const ControlMenu = ({ value, onChange, optionList }) => {
@@ -19,9 +36,18 @@ const ControlMenu = ({ value, onChange, optionList }) => {
 
 const DiaryList = ({ diaryList }) => {
   const [sortType, setSortType] = useState("lastest");
+  const [filter, setFilter] = useState("all");
 
   // 정렬된 리스트를 반환
   const getProcessedDiaryList = () => {
+    // 필터의 조건을 가지고 리턴 값 반환 해주는 콜백 함수
+    const filterCallBack = (item) => {
+      if (filter === "good") {
+        return parseInt(item.emotion) <= 3;
+      } else {
+        return parseInt(item.emotion) > 3;
+      }
+    };
     const compare = (a, b) => {
       if (sortType === "latest") {
         return parseInt(b.date) - parseInt(a.date);
@@ -31,7 +57,10 @@ const DiaryList = ({ diaryList }) => {
     };
 
     const copyList = JSON.parse(JSON.stringify(diaryList)); // 깊은 복사
-    const sortedList = copyList.sort(compare);
+
+    const filteredList =
+      filter === "all" ? copyList : copyList.filter((it) => filterCallBack(it));
+    const sortedList = filteredList.sort(compare);
     return sortedList;
   };
 
@@ -42,8 +71,15 @@ const DiaryList = ({ diaryList }) => {
         onChange={setSortType}
         optionList={sortOptionList}
       />
+      <ControlMenu
+        value={filter}
+        onChange={setFilter}
+        optionList={filterOptionList}
+      />
       {getProcessedDiaryList().map((it) => (
-        <div key={it.id}>{it.content}</div>
+        <div key={it.id}>
+          {it.content} {it.emotion}
+        </div>
       ))}
     </div>
   );
