ac6dfcc827a5749b255cfee330748f45757bbf3e
JEONGMIN <gyflsakfn@naver.com>
2022-10-15 18:42:43 +0900

    get localData

--- src/components/DiaryEditor.js
+++ src/components/DiaryEditor.js
@@ -19,7 +19,7 @@ const DiaryEditor = ({ isEdit, originData }) => {
   const navigate = useNavigate();
 
   // App.js에 있는 DiaryDispatchContext로 부터 onCreate 함수를 받아옴
-  const { onCreate, onEdit } = useContext(DiaryDispatchContext);
+  const { onCreate, onEdit, onRemove } = useContext(DiaryDispatchContext);
 
   // 선택한 감성의 상태 값 가지기
   const handleClickEmote = (emotion) => {
@@ -47,6 +47,13 @@ const DiaryEditor = ({ isEdit, originData }) => {
     navigate("/", { replace: true });
   };
 
+  const handleRemove = () => {
+    if (window.confirm("정말 삭제하시겠습니까?")) {
+      onRemove(originData.id);
+      navigate("/", { replace: true });
+    }
+  };
+
   useEffect(() => {
     if (isEdit) {
       setDate(getStringDate(new Date(parseInt(originData.date))));
@@ -60,6 +67,15 @@ const DiaryEditor = ({ isEdit, originData }) => {
       <MyHeader
         headText={isEdit ? "일기 수정하기" : "새 일기쓰기"}
         leftChild={<MyButton text={"뒤로가기"} onClick={() => navigate(-1)} />}
+        rightChild={
+          isEdit && (
+            <MyButton
+              text={"삭제하기"}
+              type={"negative"}
+              onClick={handleRemove}
+            />
+          )
+        }
       />
       <div>
         <section>
--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -23,7 +23,7 @@ const Edit = () => {
         navigate("/", { replace: true });
       }
     }
-  }, [id, diaryList]);
+  }, [id, diaryList, navigate]);
 
   return (
     <div>
